e2eaf211282635cd550bbad73887c386e7700154
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-26 23:24:29 +0100

    created index file for all components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Banner from "./layouts/Banner";
+import Navbar from "./layouts/Navbar";
+import ServiceCard from "./layouts/ServiceCard";
+import Services from "./layouts/Services";
+
+export { Navbar, Banner, Services }
