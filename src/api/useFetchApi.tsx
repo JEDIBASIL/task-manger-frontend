55aa03fe75a19bcc20a97bfbd4077e4f11ae28f0
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-29 23:57:47 +0100

    added a function and custom hook to handle api request

---
+++ src/api/useFetchApi.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import axios, { AxiosError } from "axios"
+import ApiState from '../interface/api.interface';
+
+interface AxiosConfig {
+    method: 'get' | 'post' | 'put' | 'delete' | 'patch';
+    url: string;
+    headers?: object;
+    params?: object;
+    data?: object;
+}
+
+
+export const useApi = ({ method, url, headers, data }: AxiosConfig) => {
+    const [state, setState] = useState<ApiState>({
+        data: null,
+        loading: true,
+        error: null
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setState({ data, loading: false, error: null });
+            try {
+                const response = await axios[method](url, data, headers);
+                const fetchedData = response.data;
+                setState({ data: fetchedData, loading: false, error: null });
+            } catch (error: unknown) {
+                if (error instanceof Error)
+                    setState({ data: null, loading: false, error });
+            }
+        };
+
+        fetchData();
+    }, [url]);
+
+    return state;
+};
+
+export const requestHandler = ({ method, url, headers, data }: AxiosConfig, setResponse: React.Dispatch<React.SetStateAction<ApiState>>) => {
+    setResponse({ data: {}, loading: true, error: null });
+    axios[method](url, data, headers)
+        .then(res => {
+            const fetchedData = res.data;
+            setResponse({ data: fetchedData, loading: false, error: null });
+            console.log(res.data)
+        })
+        .catch(e => {
+            if (e instanceof AxiosError)
+                setResponse({ data: null, loading: false, error: e?.response });
+        })
+}
