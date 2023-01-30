5280544455a4abd0f0eb0be7a2415872838787ef
JEDIBASIL <jedidiahbasil@gmail.com>
2023-01-30 12:33:35 +0100

    consumed password for send reset password mail

--- src/pages/auth/ForgotPassword.tsx
+++ src/pages/auth/ForgotPassword.tsx
@@ -1,19 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, PasswordInput, TextInput } from "@mantine/core";
 import { Navbar } from "../../components";
-import { Link as A } from "react-router-dom"
+import { Link as A, useNavigate } from "react-router-dom"
+import { useForm, zodResolver } from '@mantine/form';
+import { requestHandler } from '../../api/useFetchApi';
+import { forgotPasswordEmailSchema, signInSchema } from '../../schema';
+import ApiState from '../../interface/api.interface';
+import { showNotification } from "@mantine/notifications";
 
 
-const ForgotPassword: React.FC = ({  }) => {
+const ForgotPassword: React.FC = ({ }) => {
+    const navigate = useNavigate();
+
+    const [state, setState] = useState<ApiState>({
+        data: null,
+        loading: false,
+        error: null
+    });
+    const form = useForm({
+        validate: zodResolver(forgotPasswordEmailSchema),
+        initialValues: {
+            email: "",
+        },
+    });
+
+    const handleSubmit = (user: any) => {
+        let userEmail = {
+            email: user.email,
+        }
+        console.log(userEmail)
+        requestHandler(
+            {
+                method: "post",
+                url: "http://localhost:8084/api/v1/user/password-mail",
+                data: userEmail
+            },
+            setState
+        )
+
+    }
+    useEffect(() => {
+        console.log(state)
+        if (state.error?.data.includes("mail")) showNotification({
+            title: 'Failed',
+            message: 'an error occurred',
+        })
+        if (state?.data?.status && state.data?.message.includes("sent")) {
+            navigate("/sent");
+        }
+    }, [state.data, state.error])
     return (
         <>
             <Navbar logo />
             <div className={"form_content"}>
                 <h1>Forgot
                     Password</h1>
-                <TextInput mb={15} placeholder={"Email"} size={"lg"} type={"email"} />
-                <div className='forgot_password_container'><A to="/sign-in">Login</A></div>
-                <Button size={"lg"} variant="gradient" gradient={{ from: 'rgb(16, 152, 173) ', to: 'rgb(28, 126, 214)', deg: 105 }}>Reset password</Button>
+                <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+                    <TextInput  {...form.getInputProps('email')} placeholder={"Email"} size={"lg"} type={"email"} />
+                    <div className='forgot_password_container'><A to="/sign-in">Login</A></div>
+                    <Button loading={state.loading} type={"submit"} size={"lg"} >Reset password</Button>
+                </form>
             </div>
         </>
     );
